ff5fd581dbe269901aa5465673032c503f1c61d7
tonkla2548 <67041305101@udru.ac.th>
2024-11-13 01:01:51 +0700

    save

---
+++ 001char.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char name;
+    cout << "Who is the thief between mr.A and mr.B?" <<endl;
+    cin >> name;
+    cout << " Be a " << name << " thief ";
+
+}
+    
